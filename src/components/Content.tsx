1d08f8956e8a2013458694428691ccb8057985a2
Samuel Souza <samuellrrs@gmail.com>
2021-03-16 23:58:35 -0300

    feat: Adição dos componentes

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,33 @@
-export function Content() {
-  // Complete aqui
+import { Button } from "./Button";
+
+interface ContentProps {
+  selectedGenre: {
+    title: String;
+  };
+}
+
+export function Content(props: ContentProps) {
+  return (
+    // Complete aqui
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {props.selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {/*  {movies.map((movie) => (
+          <MovieCard
+          title={movie.Title}
+          poster={movie.Poster}
+          runtime={movie.Runtime}
+          rating={movie.Ratings[0].Value}
+          /> 
+        ))}*/}
+        </div>
+      </main>
+    </div>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,21 @@
-export function SideBar() {
+import { Button } from "./Button";
+export function SideBar({ genres, id, title, iconName, onClick, selected }) {
   // Complete aqui
+  <nav className="sidebar">
+    <span>
+      Watch<p>Me</p>
+    </span>
+
+    <div className="buttons-container">
+      {genres.map((genre) => (
+        <Button
+          id={id}
+          title={title}
+          iconName={iconName}
+          onClick={onClick}
+          selected={selected}
+        />
+      ))}
+    </div>
+  </nav>;
 }
